712fd269a9877af4539b984c9f0d180c41008559
Sebastian <20100835@ue.edu.pe>
2024-11-09 12:49:20 -0500

    tercer commit

--- app/src/main/java/com/AVION/pc2_/MainActivity.kt
+++ app/src/main/java/com/AVION/pc2_/MainActivity.kt
@@ -53,7 +53,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
             ), drawerLayout
         )
-        //recyclerView = findViewById(R.id.recyclerView)
+        recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
         itemAdapter = ItemAdapter(itemList)
         recyclerView.adapter = itemAdapter
